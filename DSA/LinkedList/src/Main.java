5599272f6a8f9f49bb2d698d20454af75a31b7b9
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-03 00:26:50 +0530

    Adding linked List into DSA

--- DSA/LinkedList/src/Main.java
+++ DSA/LinkedList/src/Main.java
@@ -2,5 +2,21 @@
  public class Main {
     public static void main(String[] args) {
 
+        LinkedList myLinkedList = new LinkedList(1);
+
+        myLinkedList.append(2);
+
+//        myLinkedList.prepend(1);
+
+//        myLinkedList.printList();
+
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.removeFirst());
+
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast());
+
     }
 }
