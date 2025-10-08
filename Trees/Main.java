2a1b3bb27ab3abb7f6f159c061f6970fb93c290b
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-10-08 10:08:05 +0530

    feat: added BST Main.java

---
+++ Trees/Main.java
@@ -0,0 +1,30 @@
+package Trees;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("root = " + myBST.root);
+
+        System.out.println("\nInserting Nodes...");
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+        System.out.println(myBST.root.left.right.value);
+
+        myBST.traverseTree();
+
+        System.out.println("\nSearching node for value: 27...");
+        System.out.println(myBST.contains(27));
+
+        System.out.println("\nSearching node for value: 17...");
+        System.out.println(myBST.contains(17));
+
+    }
+}
